043f3d9aec1d80d86c83678fc95c8ea13e06677b
Tibor Tálosi <teeebor@gmail.com>
2023-06-16 21:02:06 +0200

    added poly collider added RemoveComponent new Vector operators sprite scaling does not work properly

--- jumper.cpp
+++ jumper.cpp
@@ -1,30 +1,39 @@
 #include <furi.h>
-#include "defines.h"
 #include "flipper-game-engine/game-engine.h"
 #include "jumper_icons.h"
 
 
 class RotationComponent : public Component<RotationComponent> {
-    float speed=1;
+    float speed = 1;
 public:
-    explicit RotationComponent(float s): speed(s){}
-    RotationComponent(): speed(1){}
+    explicit RotationComponent(float s) : speed(s) {}
+
+    RotationComponent() : speed(1) {}
 
     void Update(const float &delta) override {
-        entity->get_transform()->rotate(speed * (float)delta);
+        entity->get_transform()->rotate(speed * (float) delta);
     }
 
-    void Swap(){
-        speed*=-1;
+    void Swap() {
+        speed *= -1;
     }
-   /* void OnInput(InputKey key, InputState type) override {
-        if(key == InputKeyOk && type == InputPress)
-            speed*=-1;
-    }
-*/
 };
 
-class SwapRotationComponent : public Component<SwapRotationComponent>{
+class ScaleComponent : public Component<ScaleComponent> {
+    float scale = 1;
+public:
+    void OnInput(InputKey key, InputState type) override {
+        if (key == InputKeyUp && type == InputPress) {
+            scale += 0.5f;
+            entity->get_transform()->set_scale(scale);
+        } else if (key == InputKeyDown && type == InputPress) {
+            scale -= 0.5f;
+            entity->get_transform()->set_scale(scale);
+        }
+    }
+};
+
+class SwapRotationComponent : public Component<SwapRotationComponent> {
     RotationComponent *component = nullptr;
 public:
     void Start() override {
@@ -32,16 +41,16 @@ public:
     }
 
     void OnInput(InputKey key, InputState type) override {
-        if(key == InputKeyOk && type == InputPress)
+        if (key == InputKeyOk && type == InputPress)
             component->Swap();
     }
 };
 
 
-class CheckHeapComponent : public Component<CheckHeapComponent>{
+class CheckHeapComponent : public Component<CheckHeapComponent> {
 public:
     void OnInput(InputKey key, InputState type) override {
-        if(key == InputKeyUp && type == InputPress)
+        if (key == InputKeyUp && type == InputPress)
             CHECK_HEAP();
     }
 };
@@ -63,15 +72,22 @@ int32_t jumper_app(void *p) {
     ball->AddComponent<RotationComponent>();
     ball->AddComponent<CheckHeapComponent>();
     ball->set_sprite(&I_platforms);
-    ball->get_transform()->set_position({20,32});
+    ball->get_transform()->set_position({20, 32});
     current_scene->Add(ball);
 
     ball = new Entity("Ball 2");
+    ball->AddComponent<RotationComponent>();
+    ball->AddComponent<ScaleComponent>();
+    ball->set_sprite(&I_big_ball);
+    ball->get_transform()->set_position({64, 16});
+    current_scene->Add(ball);
+
+    ball = new Entity("Ball 3");
     ball->AddComponent<RotationComponent>(0.5f);
     ball->AddComponent<SwapRotationComponent>();
     ball->set_sprite(&I_platforms);
-    ball->AddComponent<PhysicsBody>((Vector){1,0}, 1, Material_CONCRETE, false);
-    ball->get_transform()->set_position({100,32});
+//    ball->AddComponent<PhysicsBody>((Vector){1,0}, 1, Material_CONCRETE, false);
+    ball->get_transform()->set_position({100, 32});
     current_scene->Add(ball);
 
     engine_instance->SetScene(current_scene);
